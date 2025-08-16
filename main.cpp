548b6614ca1e119dae51a63d1405ef89c3297da9
muntasim <muntasim907@gmail.com>
2025-08-16 23:24:57 +0530

    initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+int return5(){
+   std::cout << 5 << std::endl;
+  return 0;  
+}
+
+
+using namespace std;
+int main(){
+    
+   std::cout << "hello world"<< std::endl;
+   return5();
+    return 0;
+}
+
+

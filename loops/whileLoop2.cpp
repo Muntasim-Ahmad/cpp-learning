dcd6056cfe5b29ed6da50acab0a1344e3f00bca4
muntasim <muntasim907@gmail.com>
2025-09-11 23:20:46 +0530

    adbc

---
+++ loops/whileLoop2.cpp
@@ -0,0 +1,23 @@
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int i = 1;
+//     while (i <= 10)
+//     {
+//         cout << i << " ";
+//         i++;
+//     }
+// }
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    (for char c='A'; c<='Z'; c++)
+    {
+        cout << c >> " ";
+    }
+}

1a4b09a8fee3190c646d20746886484453f06712
muntasim <muntasim907@gmail.com>
2025-09-15 22:35:00 +0530

    arrays

---
+++ loops/arrays/arr2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+// int main()
+// {
+
+//     int ar[5] = {10, 20, 30, 40, 50};
+//     for (int i = 0; i < 5; i++)
+//     {
+//         cout << ar[i] << endl;
+//     }
+// }
+
+int main()
+{
+
+    char ar[5] = {'a', 'b', 'c', 'd', 'E'};
+    for (int i = 0; i < 5; i++)
+    {
+        cout << ar[i] << endl;
+    }
+}

a4a0a9eed2652641868c80117454f7f8155fd080
muntasim <muntasim907@gmail.com>
2025-09-18 23:05:54 +0530

    smallest number

--- arrays/larestElement.cpp
+++ arrays/larestElement.cpp
@@ -1,17 +1,35 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main()
+// {
+//     int arr[5] = {50, 32, 56, 23, 665};
+//     int max = arr[0];
+//     for (int i = 1; i < 5; i++)
+//     {
+//         if (arr[i] > max)
+//         {
+//             max = arr[i];
+//         }
+//     }
+//     cout << "largest element=" << max << endl;
+// }
+
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    int arr[5] = {50, 32, 56, 23, 665};
-    int max = arr[0];
+    int arr[5] = {52, 65, 22, 11, 3};
+    int min = arr[0];
     for (int i = 1; i < 5; i++)
     {
-        if (arr[i] > max)
+        if (arr[i] < min)
         {
-            max = arr[i];
+            min = arr[i];
         }
     }
-    cout << "largest element=" << max << endl;
+    cout << "smallest element=" << min << endl;
 }

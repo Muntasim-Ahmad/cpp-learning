bb9c9409d7631123a387e8ac384b06c08028d7e8
muntasim <muntasim907@gmail.com>
2025-09-23 23:29:38 +0530

    function 1

---
+++ functions/function1.cpp
@@ -0,0 +1,29 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int add(int a, int b)
+// {
+//     return a + b;
+// }
+
+// int main()
+// {
+//     cout << "sum:" << add(55, 45) << endl;
+//     return 0;
+// }
+
+#include <iostream>
+
+using namespace std;
+
+bool iseven(int n)
+{
+    return (n % 2 == 0);
+}
+
+int main()
+{
+    int n = 55;
+    cout << n << "is" << (iseven(n) ? "even" : "odd") << endl;
+}
